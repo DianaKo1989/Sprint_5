30c27bc343959dcbe22ed5d1fcbacf2e990237f7
Diana Kozhemyakina <diko19891204@gmail.com>
2024-04-23 15:35:26 +0700

    Merge remote-tracking branch 'origin/develop' into develop

---
+++ tests/test_auth_form.py
@@ -0,0 +1,34 @@
+from selenium.webdriver.common.by import By
+from helpers.locators import (
+    lk_btn, 
+    login_btn, 
+    enter_lbl,
+    signin_btn,
+    recovery_pass_lbl,
+    recover_pass_btn,
+    signup_btn,
+    register_lbl
+)
+
+class TestAuthForm:
+    def test_enter_via_enter_button(self, wb):
+        wb.find_element(By.XPATH, signin_btn).click()
+        assert wb.find_element(By.XPATH, enter_lbl), 'Страница входа недоступна'
+
+    def test_enter_via_enter_cabinet(self, wb):
+        wb.find_element(By.XPATH, lk_btn).click()
+        assert wb.find_element(By.XPATH, enter_lbl), 'Страница входа недоступна'
+
+    def test_enter_via_enter_registration_form(self, wb):
+        wb.find_element(By.XPATH, lk_btn).click()
+        wb.find_element(By.XPATH, recover_pass_btn).click()
+        assert wb.find_element(By.XPATH, recovery_pass_lbl), 'Страница восстановления пароля недоступна'
+        wb.find_element(By.XPATH, login_btn).click()
+        assert wb.find_element(By.XPATH, enter_lbl), 'Страница входа недоступна'
+
+    def test_enter_via_enter_restore_pass(self, wb):
+        wb.find_element(By.XPATH, lk_btn).click()
+        wb.find_element(By.XPATH, signup_btn).click()
+        assert wb.find_element(By.XPATH, register_lbl), 'Страница регистрации недоступна'    
+        wb.find_element(By.XPATH, login_btn).click()
+        assert wb.find_element(By.XPATH, enter_lbl), 'Страница входа недоступна'
